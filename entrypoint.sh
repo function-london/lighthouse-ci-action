b401916a39090a72e2c7bd7192837e386e3f1e68
Peter O'Callaghan <pete@function.london>
2022-10-28 14:45:05 +0100

    🤷

--- entrypoint.sh
+++ entrypoint.sh
@@ -161,8 +161,6 @@ theme_root="${THEME_ROOT:-.}"
 
 log "Will run Lighthouse CI on $host"
 
-action_theme_id= ${{ inputs.theme_id }}
-log "action theme id: $action_theme_id"
 test=${SHOP_COLLECTION_HANDLE:-}
 log "test: $test"
 
